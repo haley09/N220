5bc067e358287b1d68bc1d70816029ec74b5330c
Haley <haley.music61@gmail.com>
2024-09-24 17:36:04 -0400

    conclusion of week 5

--- Week-5/app.js
+++ Week-5/app.js
@@ -1,4 +1,4 @@
-//Initial Object
+//Code Challenge
 let course = {
 courseName: "N220",
 courseTitle: "Introduction to Media Application Development",
@@ -15,3 +15,27 @@ course.enrolled += 2;
 console.log("Enrolled students: " + course.enrolled);
 
 console.log(course.courseName + "Open: " + course.availability());
+
+//Built-in Objects
+function IUPUIWebsite() {
+    console.log(`Inner Width: ${window.innerWidth}`);
+    console.log(`Outer Width: ${window.outerWidth}`);
+
+    console.log(window.location);
+}
+function printPage() {
+    window.print();
+}
+
+document.getElementById("dpDetails").innerHTML +=
+course.courseTitle
+
+document.querySelector("#domain").innerHTML += "HI";
+
+const updatePTag = document.getElementById("update");
+
+console.log(updatePTag.querySelector("#delete"));
+
+console.log(String(100), length, 100);
+console.log(String(100).includes("2"), 100);
+console.log(String(100).replace("1", "2"), 100);
